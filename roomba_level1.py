e3343d63218281709a6290c5fe9c7b5c29a9c231
Ali <agordon2026@kehillah.org>
2025-09-04 09:24:09 -0700

    Roomba moves!!!!!

--- roomba_level1.py
+++ roomba_level1.py
@@ -2,7 +2,7 @@
 # Roomba in Python
 # This file implements an algorithm for a roomba cleaning a room.
 #
-# Author: Dr. EB <------ REPLACE THIS WITH YOUR NAME!
+# Author: Ali <------ REPLACE THIS WITH YOUR NAME!
 # -----------------------------------------------------------------------------
  
 from turtle import left, forward, backward
@@ -13,7 +13,23 @@ window = room.draw_room(level = 1)
 
 ###
 # Start your code here
- 
+forward(160)
+left(90)
+forward(160)
+left(90)
+forward(160)
+left(90)
+forward(120)
+left(90)
+forward(120)
+left(90)
+forward(80)
+left(90)
+forward(80)
+left(90)
+forward(40)
+left(90)
+forward(40)
  
  
 # End your code here
