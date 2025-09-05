23a22feb46109e440851f0de10fb98a0b95f47c1
Ali <agordon2026@kehillah.org>
2025-09-05 11:02:58 -0700

    Roomba level 2

--- roomba_level2.py
+++ roomba_level2.py
@@ -2,7 +2,7 @@
 # Roomba in Python
 # This file implements an algorithm for a roomba cleaning a room.
 #
-# Author: Dr. EB <------ REPLACE THIS WITH YOUR NAME!
+# Author: Ali <------ REPLACE THIS WITH YOUR NAME!
 # -----------------------------------------------------------------------------
  
 from turtle import right, left, forward, backward, speed
@@ -16,9 +16,20 @@ window = room.draw_room(level = 2)
 
 ###
 # Start your code here
- 
- 
- 
+for i in range (9) :
+    forward(560)
+    left(90)
+    forward(40)
+    left(90)
+    forward(560)
+    right(90)
+    forward(40)
+    right(90)
+forward(560)
+left(90)
+forward(40)
+left(90)
+forward(560)
 # End your code here
 ###
  
