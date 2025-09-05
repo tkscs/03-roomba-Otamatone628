ac5a15de3e10ea7ad4e292c0bcf61ac6e68d805d
Ali <agordon2026@kehillah.org>
2025-09-05 11:42:12 -0700

    level 5

--- roomba_level5.py
+++ roomba_level5.py
@@ -2,7 +2,7 @@
 # Roomba in Python
 # This file implements an algorithm for a roomba cleaning a room.
 #
-# Author: Dr. EB <------ REPLACE THIS WITH YOUR NAME!
+# Author: Ali <------ REPLACE THIS WITH YOUR NAME!
 # -----------------------------------------------------------------------------
  
 from turtle import right, left, forward, backward
@@ -18,9 +18,51 @@ window = room.draw_room(level = 5, n_alcoves = n_alcoves)
 
 ###
 # Start your code here
- 
- 
- 
+forward(560)
+left(180)
+for i in range(3) :
+    forward(40)
+    left(90)
+    forward(120)
+    right(90)
+    forward(40)
+    left(90)
+    forward(40)
+    right(90)
+    forward(120)
+    right(90)
+    backward(200)
+    for i in range (4) :
+        forward(40)
+        right(90)
+        forward(40)
+        left (90)
+        forward(40)
+        left(90)
+        backward(40)
+    forward(160)
+forward(40)
+left(90)
+forward(120)
+right(90)
+forward(40)
+left(90)
+forward(40)
+right(90)
+forward(80)
+backward(80)
+right(90)
+forward(40)
+for i in range(3) :
+    forward(240)
+    left(90)
+    forward(40)
+    left(90)
+    forward(240)
+    right(90)
+    forward(40)
+    right(90) 
+forward(200)
 # End your code here
 ###
  
